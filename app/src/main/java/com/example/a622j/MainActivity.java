4c712d4be24680cd1e173d9d06b834d9330c1715
temur007222 <temurxaydarov1@gmail.com>
2022-08-29 17:58:38 +0500

    updated

--- app/src/main/java/com/example/a622j/MainActivity.java
+++ app/src/main/java/com/example/a622j/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.a622j;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,43 +24,44 @@ public class MainActivity extends AppCompatActivity {
 
     void initViews() {
         EditText email1 = findViewById(R.id.email);
-        EditText name = findViewById(R.id.name);
         EditText pas = findViewById(R.id.pas);
-        EditText cpas = findViewById(R.id.cpass);
-        Button save = findViewById(R.id.save);
+        Button save = findViewById(R.id.LogIn);
+        TextView textView = findViewById(R.id.signIn);
 
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String emailValue = email1.getText().toString();
+                String passwordValue = pas.getText().toString();
 
-                String email = email1.getText().toString();
-                PrefsManager.getInstance(MainActivity.this).saveData("key", email);
-                String data = PrefsManager.getInstance(MainActivity.this).getData("key");
-                saveEmail(data);
+                String registeredEmail = sharedPreferences.getString("emailValue", "");
+                String registeredPassword = sharedPreferences.getString("pasValue", "");
 
-                String name1 = name.getText().toString();
-                PrefsManager.getInstance(MainActivity.this).saveData("key", name1);
-                String data1 = PrefsManager.getInstance(MainActivity.this).getData("key");
-                saveEmail(data1);
+                if (emailValue.equals(registeredEmail) && passwordValue.equals(registeredPassword)){
+                    Intent intent = new Intent(MainActivity.this, LastActivity.class );
+                    startActivity(intent);
+                }else{
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
 
-                String pas1 = pas.getText().toString();
-                PrefsManager.getInstance(MainActivity.this).saveData("key", pas1);
-                String data2 = PrefsManager.getInstance(MainActivity.this).getData("key");
-                saveEmail(data2);
+            }
+        });
 
-                String cpas1 = cpas.getText().toString();
-                PrefsManager.getInstance(MainActivity.this).saveData("key", cpas1);
-                String data3 = PrefsManager.getInstance(MainActivity.this).getData("key");
-                saveEmail(data3);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
 
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LastActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    void saveEmail(String email) {
-        SharedPreferences prefs = getApplicationContext().getSharedPreferences("MyPref", MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        editor.putString("email", email);
-        editor.apply();
-    }
 }
